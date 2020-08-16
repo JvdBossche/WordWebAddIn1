d535c641ef320d8fd85b2c1a5546b4d8d2d7d541
Jochen Van den Bossche <vandej36@BTJMGC2>
2020-08-16 20:52:38 +0200

    Alles werkt t.e.m. genereren van QR in de Add-In :) Ipv png kopieren naar doc, wordt er een tekst geinserted: de verbinding met het doc werkt dus.

--- WordWebAddIn1Web/Home.js
+++ WordWebAddIn1Web/Home.js
@@ -1,106 +1,46 @@
 ﻿
+"use strict";
 (function () {
-    "use strict";
 
-    var messageBanner;
+    //var messageBanner;
 
     // The initialize function must be run each time a new page is loaded.
-    Office.initialize = function (reason) {
+    Office.onReady(function () {
+        // Office is ready
+        console.log("Office Ready");
         $(document).ready(function () {
+            /*
             // Initialize the notification mechanism and hide it
             var element = document.querySelector('.MessageBanner');
             messageBanner = new components.MessageBanner(element);
-            messageBanner.hideBanner();
-
-            // If not using Word 2016, use fallback logic.
-            if (!Office.context.requirements.isSetSupported('WordApi', '1.1')) {
-                $("#template-description").text("This sample displays the selected text.");
-                $('#button-text').text("Display!");
-                $('#button-desc').text("Display the selected text");
-                
-                $('#highlight-button').click(displaySelectedText);
-                return;
-            }
-
-            $("#template-description").text("This sample highlights the longest word in the text you have selected in the document.");
-            $('#button-text').text("Highlight!");
-            $('#button-desc').text("Highlights the longest word.");
-            
-            loadSampleData();
-
-            // Add a click event handler for the highlight button.
-            $('#highlight-button').click(hightlightLongestWord);
+            messageBanner.hideBanner();*/
+            $('#btnCopyQr').click(copyToDoc);
+            console.log("Doc ready. copyToDoc() set as click event handler");
         });
-    };
+    });    
 
-    function loadSampleData() {
-        // Run a batch operation against the Word object model.
+    function copyToDoc() {
+        console.log("in copyToDoc");
         Word.run(function (context) {
             // Create a proxy object for the document body.
-            var body = context.document.body;
-
-            // Queue a commmand to clear the contents of the body.
-            body.clear();
-            // Queue a command to insert text into the end of the Word document body.
-            body.insertText(
-                "This is a sample text inserted in the document",
-                Word.InsertLocation.end);
-
-            // Synchronize the document state by executing the queued commands, and return a promise to indicate task completion.
-            return context.sync();
-        })
-        .catch(errorHandler);
-    }
-
-    function hightlightLongestWord() {
-        Word.run(function (context) {
-            // Queue a command to get the current selection and then
-            // create a proxy range object with the results.
-            var range = context.document.getSelection();
-            
-            // This variable will keep the search results for the longest word.
-            var searchResults;
-            
-            // Queue a command to load the range selection result.
-            context.load(range, 'text');
-
-            // Synchronize the document state by executing the queued commands
+            var theDocBody = context.document.body;
+            // Queue a command to insert text at the end of the document body.
+            theDocBody.insertText("Text inserted via button.", Word.InsertLocation.end); //Word.InsertLocation.after ?
+            // Synchronize the document state by executing the queued commands,
             // and return a promise to indicate task completion.
-            return context.sync()
-                .then(function () {
-                    // Get the longest word from the selection.
-                    var words = range.text.split(/\s+/);
-                    var longestWord = words.reduce(function (word1, word2) { return word1.length > word2.length ? word1 : word2; });
-
-                    // Queue a search command.
-                    searchResults = range.search(longestWord, { matchCase: true, matchWholeWord: true });
-
-                    // Queue a commmand to load the font property of the results.
-                    context.load(searchResults, 'font');
-                })
-                .then(context.sync)
-                .then(function () {
-                    // Queue a command to highlight the search results.
-                    searchResults.items[0].font.highlightColor = '#FFFF00'; // Yellow
-                    searchResults.items[0].font.bold = true;
-                })
-                .then(context.sync);
-        })
-        .catch(errorHandler);
-    } 
-
-
-    function displaySelectedText() {
-        Office.context.document.getSelectedDataAsync(Office.CoercionType.Text,
-            function (result) {
-                if (result.status === Office.AsyncResultStatus.Succeeded) {
-                    showNotification('The selected text is:', '"' + result.value + '"');
-                } else {
-                    showNotification('Error:', result.error.message);
+            return context.sync().then(function () {
+                console.log('Text added');
+            });
+        }
+        )
+            .catch(function (error) {
+                console.log('Error: ' + JSON.stringify(error));
+                if (error instanceof OfficeExtension.Error) {
+                    console.log('Debug info: ' + JSON.stringify(error.debugInfo));
                 }
             });
     }
-
+    /*
     //$$(Helper function for treating errors, $loc_script_taskpane_home_js_comment34$)$$
     function errorHandler(error) {
         // $$(Always be sure to catch any accumulated errors that bubble up from the Word.run execution., $loc_script_taskpane_home_js_comment35$)$$
@@ -118,4 +58,5 @@
         messageBanner.showBanner();
         messageBanner.toggleExpansion();
     }
+    */
 })();
